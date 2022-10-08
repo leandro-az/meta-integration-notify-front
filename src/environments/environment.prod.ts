3380934586aefb4beb68c48c2e0138b1a02a8657
Leandro Almeida <leandro_almeida@id.uff.br>
2022-10-07 22:59:09 -0300

    update backend url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   google_client_id:
     '989386515709-4vv7tcjeu1kqj3hu57d9l5fqq77jdpr3.apps.googleusercontent.com',
     backend_base_url:
-    'http://metaintegrationnotify-env.eba-ntb4pbm2.us-east-1.elasticbeanstalk.com/graphql',
+    'https://d1jjp4ddzxq0gc.cloudfront.net/graphql',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
     '989386515709-4vv7tcjeu1kqj3hu57d9l5fqq77jdpr3.apps.googleusercontent.com',
     
   backend_base_url:
-    'http://metaintegrationnotify-env.eba-ntb4pbm2.us-east-1.elasticbeanstalk.com/graphql',
+    'https://d1jjp4ddzxq0gc.cloudfront.net/graphql',
 };
 
 /*
