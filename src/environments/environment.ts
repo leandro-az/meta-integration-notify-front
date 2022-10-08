5d702b8ff45d7a10551ca24a074b45295850c826
Leandro Almeida <leandro_almeida@id.uff.br>
2022-10-07 23:12:33 -0300

    Force deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,6 @@ export const environment = {
   production: false,
   google_client_id:
     '989386515709-4vv7tcjeu1kqj3hu57d9l5fqq77jdpr3.apps.googleusercontent.com',
-    
   backend_base_url:
     'https://d1jjp4ddzxq0gc.cloudfront.net/graphql',
 };
