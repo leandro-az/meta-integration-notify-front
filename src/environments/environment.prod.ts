b084ade0470a44a2a584a34a318197dd57557ee2
Leandro Almeida <leandro_almeida@id.uff.br>
2022-10-07 21:38:48 -0300

    add production and change gSI

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   google_client_id:
-    '989386515709-jqmkor1mdimvasko7j1cev5nilmfgura.apps.googleusercontent.com',
+    '989386515709-4vv7tcjeu1kqj3hu57d9l5fqq77jdpr3.apps.googleusercontent.com',
     backend_base_url:
     'http://metaintegrationnotify-env.eba-ntb4pbm2.us-east-1.elasticbeanstalk.com/graphql',
 };
